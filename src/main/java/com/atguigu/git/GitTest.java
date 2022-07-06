8920459a184a05ec63e813c2baa5d9d92d51fd89
xx <xx@qq.com>
2022-07-06 23:16:30 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello github");
         System.out.println("hello gittee");
         System.out.println("hello git4");
+        System.out.println("hello git4 hot ");
     }
 }
