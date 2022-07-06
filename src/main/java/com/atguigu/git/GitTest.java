a22c19768d4f4f73dfcee79e11460aec3eb06386
xx <xx@qq.com>
2022-07-06 23:44:06 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git4");
         System.out.println("hello git4 master");
         System.out.println("hello git4 hot ");
+        System.out.println("push test");
     }
 }
